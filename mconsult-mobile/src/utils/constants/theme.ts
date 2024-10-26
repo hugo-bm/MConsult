e59885fdab1d3966ebf6cf935e8beffb53ae6580
Hugo Batalha Moreno <hugomb123@gmail.com>
2024-10-26 10:14:48 -0300

    💄 feat (Style): added main app style
    The theme.tsx file is implemented using a CSS Stylesheet, containing the main colors and fonts for the application.

---
+++ mconsult-mobile/src/utils/constants/theme.ts
@@ -0,0 +1,30 @@
+const COLORS = {
+    primary: "#52a230",
+    secondary: "#6c757d",
+    success: "#388e3c",
+    danger: "#dc3545",
+    warning: "#ebc248",
+    info: "#17a2b8",
+    light: "#f8f9fa",
+    dark: "#42484d",
+
+    gray1: "#323434",
+    inputPalceholder: "#717f7f",
+    gray3: "#a0a0a0",
+    gray4: "#E4E4E4",
+    inputBackground: "#d9dddc",
+    mainBackground: ""
+}
+
+COLORS.mainBackground = COLORS.light
+
+const FONT_SIZE = {
+    xsm: 11,
+    sm: 14,
+    md: 16,
+    lg: 18,
+    xl: 32,
+    xxl: 64
+}
+
+export { COLORS, FONT_SIZE}
