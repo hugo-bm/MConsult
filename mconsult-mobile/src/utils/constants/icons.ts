5f5c9c774363ea97f95be73e46abb00334b37b34
Hugo Batalha Moreno <hugomb123@gmail.com>
2024-10-26 10:20:50 -0300

    🍱 feat (icons): added icon files to the assets folder

---
+++ mconsult-mobile/src/utils/constants/icons.ts
@@ -0,0 +1,9 @@
+const logo = require("../../assets/logo.png");
+const home = require("../../assets/home.png");
+const fdoctor = require("../../assets/female-doctor.png");
+const mdoctor = require("../../assets/doctor.png");
+const clock = require("../../assets/time.png");
+const calendar = require("../../assets/calendar.png");
+const profile = require("../../assets/user.png");
+
+export  { logo, home, fdoctor, mdoctor, calendar, clock, profile };
