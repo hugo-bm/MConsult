acabaf70673954eb654c1f739486a4e0a30f869f
Hugo Batalha Moreno <hugomb123@gmail.com>
2024-11-04 13:02:45 -0300

    ✨ feat(screens): add main screen and navigation system
    - The main screen was added, responsible for displaying the frames: Main, Consult and Profile.
    - The dependencies of the Bottom-tabs were used to navigate between the frames.
    
    Bugs with the version: In the navigation tabs container, it is not possible to add the "Main background color" behind the container, causing a break in color continuity with the application background.
    
    Temporary solution: The value of the container's "borderRound" property was reduced, reducing the perception of a break in continuity.

---
+++ mconsult-mobile/src/screens/mainScreen/mainScreen.tsx
@@ -0,0 +1,104 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import FrameHome from "../frameHome/frameHome";
+import FrameProfile from "../frameProfile/frameProfile";
+import FrameConsult from "../frameConsult/frameConsult";
+import { Image } from "react-native";
+import { calendar, home, logo, profile } from "../../utils/constants/icons";
+import { COLORS } from "../../utils/constants/theme";
+
+const Tab = createBottomTabNavigator();
+const Main = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarStyle: {
+            borderTopWidth: 1,
+            borderStartStartRadius: 10,
+            borderStartEndRadius: 10,
+          },
+
+          headerStyle: {
+            backgroundColor: COLORS.mainBackground,
+            borderWidth: 0,
+          },
+        }}
+      >
+        <Tab.Screen
+          name="Inicio"
+          component={FrameHome}
+          options={{
+            headerTitleAlign: "center",
+            headerTitle: function () {
+              return <Image source={logo} />;
+            },
+            tabBarShowLabel: false,
+            tabBarIcon: function ({ focused }) {
+              return (
+                <Image
+                  source={home}
+                  style={{
+                    width: 25,
+                    height: 25,
+                    opacity: focused ? 1 : 0.34,
+                    objectFit: "cover",
+                  }}
+                />
+              );
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Consultas"
+          component={FrameConsult}
+          options={{
+            headerTitleAlign: "center",
+            headerTitle: function () {
+              return <Image source={logo} />;
+            },
+            tabBarShowLabel: false,
+            tabBarIcon: function ({ focused }) {
+              return (
+                <Image
+                  source={calendar}
+                  style={{
+                    width: 25,
+                    height: 25,
+                    opacity: focused ? 1 : 0.34,
+                    objectFit: "cover",
+                  }}
+                />
+              );
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Perfil"
+          component={FrameProfile}
+          options={{
+            headerTitleAlign: "center",
+            headerTitle: function () {
+              return <Image source={logo} />;
+            },
+            tabBarShowLabel: false,
+            tabBarIcon: function ({ focused }) {
+              return (
+                <Image
+                  source={profile}
+                  style={{
+                    width: 25,
+                    height: 25,
+                    opacity: focused ? 1 : 0.34,
+                    objectFit: "cover",
+                  }}
+                />
+              );
+            },
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+export default Main;
