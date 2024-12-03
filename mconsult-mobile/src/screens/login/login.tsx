ef6e1b9b9737f97c3696aa1013458546f74a9484
Hugo Batalha Moreno <hugomb123@gmail.com>
2024-12-03 17:07:27 -0300

    ✨feat(screen): implemented logic for user login
    - The collection of information and use of the "login" function of the authentication context to perform the login process has been implemented.

--- mconsult-mobile/src/screens/login/login.tsx
+++ mconsult-mobile/src/screens/login/login.tsx
@@ -1,29 +1,48 @@
-import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Alert, Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import styles from "./login.style";
 import Button from "../../components/button/button";
-import {logo} from "../../utils/constants/icons"
+import { logo } from "../../utils/constants/icons";
+import { useContext, useState } from "react";
+import api from "../../utils/constants/api";
+import { AuthContext } from "../../utils/context/auth";
 
-const Login = (): React.JSX.Element => {
+
+const Login = (props: {navigation: any}): React.JSX.Element => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const auth = useContext(AuthContext);
+
+  async function ExecuteLogin() {
+    await auth.login({email, password});
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.containerLogo}>
-        <Image
-          source={logo}
-          style={styles.logo}
-        />
+        <Image source={logo} style={styles.logo} />
       </View>
       <View>
         <View style={styles.containerInput}>
-          <TextInput placeholder="E-mail" style={styles.input} />
+          <TextInput placeholder="E-mail" onChangeText={(t)=>setEmail(t)} style={styles.input} />
         </View>
         <View style={styles.containerInput}>
-          <TextInput placeholder="Senha" style={styles.input} secureTextEntry={true} />
+          <TextInput
+            placeholder="Senha"
+            style={styles.input}
+            secureTextEntry={true}
+            onChangeText={(t)=>setPassword(t)}
+          />
         </View>
-        <Button text="Acessar" outline={false} theme="primary" />
+        <Button
+          text="Acessar"
+          outline={false}
+          theme="primary"
+          onPress={ExecuteLogin}
+        />
       </View>
       <View style={styles.footer}>
         <Text style={styles.footerText}>Não tenho conta. </Text>
-        <TouchableOpacity >
+        <TouchableOpacity onPress={()=>props.navigation.navigate("SignUp")}>
           <Text style={styles.Link}>Criar conta agora</Text>
         </TouchableOpacity>
       </View>
