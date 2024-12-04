97379c1003b5a3504fe8f865cc647d1ebf5cb89e
Hugo Batalha Moreno <hugomb123@gmail.com>
2024-12-04 19:49:18 -0300

    ✨feat(component): Updated Doctor List item component
    The ability to execute a function passed as a reference via parameter has been implemented. The responsibility for accessing the icon has been passed to this component, receiving a character F or M via parameter, being of type "string". The doctor's ID has been passed as a parameter and used as a parameter of the function passed in "onPress".
    
    The purpose of this component is to render a component that displays the doctor's information and executes an action when pressed.

--- mconsult-mobile/src/components/DoctorListItem/doctorlistitem.tsx
+++ mconsult-mobile/src/components/DoctorListItem/doctorlistitem.tsx
@@ -1,18 +1,21 @@
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import styles from "./doctorlistitem.style";
+import { mdoctor, fdoctor } from "../../utils/constants/icons";
 
 
 const DoctorListItem = (props: {
+  id_doctor: string;
   name: string;
-  icon: any;
-  speciality: string;
+  icon: "F" | "M";
+  specialty: string;
+  onPress: Function;
 }) => {
   return (
-    <TouchableOpacity style={styles.doctor}>
-      <Image source={props.icon} style={styles.icon}/>
+    <TouchableOpacity style={styles.doctor} onPress={() => props.onPress(props.id_doctor, props.name, props.specialty, props.icon)}>
+      <Image source={props.icon == "F" ? fdoctor : mdoctor} style={styles.icon}/>
       <View>
         <Text style={styles.name}>{props.name}</Text>
-        <Text style={styles.speciality}>{props.speciality}</Text>
+        <Text style={styles.speciality}>{props.specialty}</Text>
       </View>
     </TouchableOpacity>
   );
