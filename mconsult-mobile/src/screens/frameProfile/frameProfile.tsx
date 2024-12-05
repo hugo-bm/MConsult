747910327fc12aca1433ad9f58594dbdfeedf952
Hugo Batalha Moreno <hugomb123@gmail.com>
2024-12-05 12:09:13 -0300

    ✨feat(screen): Updated Profile frame
    The ability to obtain data from the server via request has been implemented. If the user loading process is completed successfully, the status is updated from "undefined" to the userProfile type (defined momentarily in the file itself). The userProfile status is used to define whether the information or an informative message will be displayed. The process is repeated every time the component is assembled.
    
    The "Sair" button has been added to perform the logout process by obtaining the function from the authentication context and passing it as the value of the "onPress" property.
    
    The screen component's style file has received the addition of a style for the message displayed when an error occurs when obtaining doctors.

--- mconsult-mobile/src/screens/frameProfile/frameProfile.tsx
+++ mconsult-mobile/src/screens/frameProfile/frameProfile.tsx
@@ -1,25 +1,69 @@
 import { Text, View } from "react-native";
 import styles from "./frameProfile.style";
+import api from "../../utils/constants/api";
+import { useContext, useEffect, useState } from "react";
+import Button from "../../components/button/button";
+import { AuthContext } from "../../utils/context/auth";
+
+type userProfile = {
+  name: string;
+  birthDate: Date;
+  email: string;
+  mobile: string;
+};
 
 const FrameProfile = () => {
-return <View style={styles.container}>
-    <View style={styles.item}>
-        <Text style={styles.title}>Nome</Text>
-        <Text style={styles.text}>Fulano de Tal</Text>
+  const [user, setUser] = useState<userProfile>();
+
+  const {logout} = useContext(AuthContext);
+
+  async function getUserInfo() {
+    const resp = await api.get("/user");
+    const { name, birth_date, email, phone } = resp.data.data;
+    setUser({
+      name,
+      birthDate: new Date(birth_date),
+      email: email.value,
+      mobile: phone,
+    });
+  }
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {user ? (
+        <>
+          <View style={styles.item}>
+            <Text style={styles.title}>Nome</Text>
+            <Text style={styles.text}>{user ? user.name : ""}</Text>
+          </View>
+          <View style={styles.item}>
+            <Text style={styles.title}>Data nascimento</Text>
+            <Text style={styles.text}>
+              {user
+                ? new Intl.DateTimeFormat("pt-BR").format(user.birthDate)
+                : ""}
+            </Text>
+          </View>
+          <View style={styles.item}>
+            <Text style={styles.title}>E,mail</Text>
+            <Text style={styles.text}>{user ? user.email : ""}</Text>
+          </View>
+          <View style={styles.item}>
+            <Text style={styles.title}>Telefone</Text>
+            <Text style={styles.text}>{user ? user.mobile : ""}</Text>
+          </View>
+          <View style={styles.item}>
+            <Button text={"Sair"} outline={false} theme={"danger"} onPress={()=>logout()}/>
+          </View>
+        </>
+      ) : (
+        <Text>Perfil não encontrado</Text>
+      )}
     </View>
-    <View style={styles.item}>
-        <Text style={styles.title}>Data nascimento</Text>
-        <Text style={styles.text}>20/20/1989</Text>
-    </View>
-    <View style={styles.item}>
-        <Text style={styles.title}>Email</Text>
-        <Text style={styles.text}>Fulano@email.com</Text>
-    </View>
-    <View style={styles.item}>
-        <Text style={styles.title}>Telefone</Text>
-        <Text style={styles.text}>(99) 99999-9999</Text>
-    </View>
-</View>
+  );
 };
 
 export default FrameProfile;
